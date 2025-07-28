55c8780733eb57cfc24af1ee22efad4bf126c286
ISTKHAROP <istkharalam2006@gmail.com>
2025-07-29 00:02:56 +0530

    Update mustjoin.py

--- YTMUSIC/plugins/bot/mustjoin.py
+++ YTMUSIC/plugins/bot/mustjoin.py
@@ -5,7 +5,7 @@ from YTMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "THUNDERDEVS"
+MUST_JOIN = "ll_ISTKHAR_ll"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
